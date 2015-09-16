1d4f622bc88546212d69c866c8852caef3bc0bae
Allan Zyne <allanzyne@icloud.com>
2015-09-17 00:23:19 +0800

    add files

---
+++ gainian.js
@@ -0,0 +1,38 @@
+var epub = require(epub);
+
+
+// zip 
+// 
+
+zip.open();
+
+zip.addFile(dst, src);
+zip.deleteFile(dst);
+
+
+zip.write();
+
+
+// 
+// epub
+
+
+// META-INF
+// 
+
+
+// NCX
+// 
+
+
+// OPF
+//
+
+
+
+
+// markdown
+//
+
+
+
